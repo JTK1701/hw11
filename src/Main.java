d70bfcfaa4a61c1af144e409b9de84a5ac84061e
JTK1701 <jonmalkon@mail.ru>
2024-04-16 17:01:16 +0300

    hw11

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("1 задача");
-        System.out.println("Какой сейчас год?");
+        System.out.println("Введите любой год после 1584");
         Scanner tyk = new Scanner(System.in);
         int year = tyk.nextInt();
         String yearNow = checkLeapYear(year);
-
+        System.out.println(yearNow);
+        //2
         System.out.println("");
         System.out.println("");
         System.out.println("2 задача");
         System.out.println("Если у вас android введите 1, если apple, введите 0");
         Scanner osIn = new Scanner(System.in);
         int os2 = osIn.nextInt();
+        if (os2 > 1) {
+            throw new RuntimeException("Вы ввели неверные данные ОС.");
+        }
         System.out.println("Уточните, пожалуйста, год выпуска вашего мобильного устройства.");
         Scanner y = new Scanner(System.in);
         int yearOs2 = y.nextInt();
+        if (yearOs2 < 1990 || yearOs2 > 2024) {
+            throw new RuntimeException("Вы ввели неверный год.");
+        }
         String message = selectMessage(yearOs2, os2);
         System.out.println(message);
-
+        //3
         System.out.println("");
         System.out.println("");
         System.out.println("3 задача");
         System.out.println("Пожалуйста, укажите дистанцию от вашего дома до отделения банка.");
         Scanner km = new Scanner(System.in);
         int deliveryDistance = km.nextInt();
-        String answ = calculateDeliveryTime(deliveryDistance);
-        System.out.println(answ);
+        String mess = "";
+        if (deliveryDistance < 0) {
+            throw new RuntimeException("Вы ввели неверные данные");
+        }
+        int answ = calculateDeliveryTime(deliveryDistance);
+        if (answ < 4) {
+            mess = "Доставка займет дней - " + answ;
+        } else if (answ == 4) {
+            mess = "Извините, доставка в зону свыше 100 км не осуществляется.";
+        }
+        System.out.println(mess);
     }
 
     //метод первой задачи
@@ -45,7 +61,6 @@ public class Main {
         } else if (year % b == 0 || year % c == 0 && year > 1584) {
             nowYear = year + " - год является високосным.";
         }
-        System.out.printf(nowYear);
         return nowYear;
     }
 
@@ -62,29 +77,24 @@ public class Main {
             messageForUser = messageForUser + "для iOS по ссылке.";
         } else if (os == 1) {
             messageForUser = messageForUser + "для android по ссылке.";
-        } else if (os > 1) {
-            throw new RuntimeException("Вы ввели неверные данные ОС.");
-        } else if (yearOs < 1990 || yearOs > currentYear) {
-            throw new RuntimeException("Вы ввели неверный год.");
         }
         return messageForUser;
     }
 
     //Метод третьей задачи
-    public static String calculateDeliveryTime(int distance) {
-        String answer = "";
+    public static int calculateDeliveryTime(int distance) {
+        int answer = 0;
         if (distance <= 20) {
-            answer = "Доставка займет 1 день.";
+            answer = 1;
         } else if (distance > 20 && distance <= 60) {
-            answer = "Доставка займет 2 дня.";
+            answer = 2;
         } else if (distance > 60 && distance <= 100) {
-            answer = "Доставка займет 3 дня.";
+            answer = 3;
         } else if (distance > 100) {
-            answer = "Извините, мы не осуществляем доставку карт свыше 100 километров.";
-        } else if (distance < 0) {
-            throw new RuntimeException("Вы ввели неверные данные");
+            answer = 4;
         }
         return answer;
     }
+    //Герман, привет еще раз. А тут у меня, почему-то именно Ctrl+Alt+L делает отступы между строками в методах. Так и должно быть?
 }
 
